fed44288390277fc00b7872bdbebf0bfce117bcd
haochiencho@gmail.com <haochiencho@gmail.com>
2017-02-22 11:30:02 -0800

    added getInput to user interface controller

--- app.js
+++ app.js
@@ -4,16 +4,41 @@ var BudgetController = (function(){
 
 var UIController = (function(){
 	// User Interface controller code
+	var DOMstrings = {
+		inputType: '.add__type',
+		inputDescription: '.add__description',
+		inputAmount: '.add__value',
+		inputBtn: '.add__btn'
+	}
+
+
+	return {
+		getInput: function(){
+			return {
+				// type will be 'income' or 'expense'
+				type: document.querySelector(DOMstrings.inputType).value,
+				description: document.querySelector(DOMstrings.inputDescription).value,
+				amount: document.querySelector(DOMstrings.inputAmount).value
+			}
+		},
+
+		getDOMstrings: function(){
+			return DOMstrings;
+		}
+	}
 })();
 
 var AppController = (function(BudgetCtrl, UICtrl){
 	// Application controller
+	var DOM = UICtrl.getDOMstrings();
 
-	function controlAddItem(){
-		console.log('e');
+	var controlAddItem = function(){
+		// get input
+		var input = UICtrl.getInput();
+		console.log(input);
 	}
 
-	document.querySelector('.add__btn').addEventListener('click', controlAddItem);
+	document.querySelector(DOM.inputBtn).addEventListener('click', controlAddItem);
 
 	document.addEventListener('keypress', function(event){
 		if(event.keyCode === 13 || event.which === 13){
