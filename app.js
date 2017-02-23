3ca1c777bb321051c8e1988a5d2dfd0c115a438e
haochiencho@gmail.com <haochiencho@gmail.com>
2017-02-22 17:31:46 -0800

    modularized budget controller into its individually components

--- app.js
+++ app.js
@@ -1,5 +1,58 @@
 var BudgetController = (function(){
 	// Budget Controller code
+
+	var Expense = function(id, description, value){
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
+
+	var Income = function(id, description, value){
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
+
+	var data = {
+		items: {
+			expense: [],
+			income: []
+		},
+		total: {
+			expense: 0,
+			income: 0
+		}
+
+	};
+
+	return {
+		addItem: function(type, des, val){
+			var newItem, ID;
+			// ID of the last element in the array + 1
+			if(data.items[type].length > 0){
+				ID = data.items[type][data.items[type].length - 1].id + 1;				
+			} else{
+				ID = 0;
+			}
+
+
+			if(type === 'expense'){
+				newItem = new Expense(ID, des, val);
+			}
+			else if (type === 'income'){
+				newItem = new Income(ID, des, val);
+			}
+			
+			data.items[type].push(newItem);
+			data.total[type] += val;
+			return newItem;
+		},
+
+		test: function(){
+			console.log(data);
+		}
+	}
+
 })();
 
 var UIController = (function(){
@@ -30,19 +83,34 @@ var UIController = (function(){
 
 var AppController = (function(BudgetCtrl, UICtrl){
 	// Application controller
-	var DOM = UICtrl.getDOMstrings();
+	var setupEventListeners = function(){
+		var DOM = UICtrl.getDOMstrings();
+
+		document.querySelector(DOM.inputBtn).addEventListener('click', controlAddItem);
+
+		document.addEventListener('keypress', function(event){
+			if(event.keyCode === 13 || event.which === 13){
+				controlAddItem();
+			}
+		});
+	}
 
 	var controlAddItem = function(){
 		// get input
 		var input = UICtrl.getInput();
+		// add item to budget controller
+		BudgetCtrl.addItem(input.type, input.description, input.amount);
 		console.log(input);
 	}
 
-	document.querySelector(DOM.inputBtn).addEventListener('click', controlAddItem);
-
-	document.addEventListener('keypress', function(event){
-		if(event.keyCode === 13 || event.which === 13){
-			controlAddItem();
+	return {
+		init: function(){
+			console.log('application has started');
+			setupEventListeners();
 		}
-	});
+	}
+
+
 })(BudgetController, UIController);
+
+AppController.init();
